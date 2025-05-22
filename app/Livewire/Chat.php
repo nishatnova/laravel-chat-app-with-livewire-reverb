1bd94f88d325ec8893566cdf49f133c34e41bb1d
Nishat Anzum Nova <nishat15-12132@diu.edu.bd>
2025-05-22 09:05:52 +0600

    sending message local

--- app/Livewire/Chat.php
+++ app/Livewire/Chat.php
@@ -4,14 +4,19 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class Chat extends Component
 {
     public $user;
     public $message;
+    public $senderId;                 
+    public $receiverId;                 
 
     public function mount($userId){
         $this->user = $this->getUsers($userId);
+        $this->senderId = Auth::user()->id;
+        $this->receiverId = $userId;
     }
     public function render()
     {
@@ -22,5 +27,19 @@ class Chat extends Component
        return User::find($userId);
     }
 
+    public function sendMessage(){
+        $this->saveMessage();
+    }
     
+    public function saveMessage(){
+        dd([
+        'sender_id' => $this->senderId,
+        'receiver_id' =>$this->receiverId,
+        'message' =>$this->message,
+        // 'file_name',
+        // 'file_original_name',
+        // 'folder_path',
+        'is_read' => false
+        ]);
+    }
 }
